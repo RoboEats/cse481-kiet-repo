49a093b6008a2956d5a70a5cc5d9f58f6c7a0be6
Kiet <kietmsm@gmail.com>
2019-01-26 19:13:29 -0800

    w3-l10 - got torso web teleop to work

--- web_teleop/nodes/actuators_node.py
+++ web_teleop/nodes/actuators_node.py
@@ -17,7 +17,10 @@ class ActuatorServer(object):
         self._torso = robot_api.Torso()
 
     def handle_set_torso(self, request):
+        # type: (SetTorsoRequest) -> None
         # TODO: move the torso to the requested height
+        print "web_teleop/set_torso: setting height to " + request.height
+        self._torso.set_height(request.height)
         return SetTorsoResponse()
 
 
@@ -25,8 +28,10 @@ def main():
     rospy.init_node('web_teleop_actuators')
     wait_for_time()
     server = ActuatorServer()
+    print "web_teleop/set_torso: starting service..."
     torso_service = rospy.Service('web_teleop/set_torso', SetTorso,
                                   server.handle_set_torso)
+    print "web_teleop/set_torso: service running..."
     rospy.spin()
 
 
